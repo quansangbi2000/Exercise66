5f65e31ee1af065e37aa19a4db8cce19e5df1c3e
quansangbi2000 <hieungghia1999@gmail.com>
2019-10-17 10:47:18 +0700

    17/10/2019

---
+++ Exercise06/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Problem1;
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitAccount
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Account acc = new Account(200000);
+
+            Assert.IsNotNull(acc);
+            Assert.IsInstanceOfType(acc, typeof(Account));
+            Assert.AreEqual(200000, acc.Balance());
+        }
+        [TestMethod]
+        public void TestMethodWithDraw()
+        {
+            Account acc = new Account(500000);
+            acc.Withdraw(200000);
+            Assert.AreEqual(300000, acc.Balance());
+            acc.Withdraw(300000);
+            Assert.AreEqual(0, acc.Balance());
+        }
+        [TestMethod]
+        public void TestMethodDeposit()
+        {
+            Account acc = new Account(500000);
+            acc.Deposit(200000);
+            Assert.AreEqual(700000, acc.Balance());
+            acc.Deposit(300000);
+            Assert.AreEqual(1000000, acc.Balance());
+        }
+        [TestMethod]
+        public void TestMethodBalance()
+        {
+            Account acc = new Account(500000);
+            acc.Deposit(200000);
+            Assert.AreEqual(700000, acc.Balance());
+            acc.Deposit(300000);
+            Assert.AreEqual(00000, acc.Balance());
+        }
+    }
+}
